9e3f04cea40749b8510bf61e9cc0d6a59ffbe3a4
mgbardakov <mgbardakov@gmail.com>
2022-05-31 13:02:56 +0600

    init project

---
+++ src/test/java/site/hornsandhooves/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hornsandhooves.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
